e2e364a000f0cb2d2f87687ee04b9cbcf7324932
Michał Gołębiowski <m.goleb@gmail.com>
2014-06-21 04:00:35 +0200

    Tabs -> spaces

--- grunt/tasks/post-uglify.js
+++ grunt/tasks/post-uglify.js
@@ -3,10 +3,10 @@
 module.exports = function (grunt) {
     // Remove 'dist/' prefixes from paths put in the map file
     grunt.registerMultiTask('post-uglify', function () {
-		this.files.forEach(function( mapping ) {
-			mapping.src.forEach( function( src ) {
-				grunt.file.write( src, grunt.file.read( src ).replace( /"dist\//g, '"' ) );
-			});
-		});
-	});
+        this.files.forEach(function (mapping) {
+            mapping.src.forEach(function (src) {
+                grunt.file.write(src, grunt.file.read(src).replace(/"dist\//g, '"'));
+            });
+        });
+    });
 };
