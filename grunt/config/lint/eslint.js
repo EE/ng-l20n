feeb541662fca7d95c916f009e24fe14e7f97132
Michał Gołębiowski <m.goleb@gmail.com>
2014-06-21 04:00:27 +0200

    Remove copyright headers

--- grunt/config/lint/eslint.js
+++ grunt/config/lint/eslint.js
@@ -1,12 +1,3 @@
-/**
- * ng-l20n
- * https://github.com/EE/ng-l20n
- *
- * Author Michał Gołębiowski <michal.golebiowski@laboratorium.ee>
- * © 2012, 2013 Laboratorium EE
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 module.exports = {
